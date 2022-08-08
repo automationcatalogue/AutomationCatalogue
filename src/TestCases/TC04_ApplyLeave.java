e6da3a8fac30e4fd0d9755454368f46fabb8b6b6
automationcatalogue <automation.catalogue@gmail.com>
2022-08-08 19:45:10 +0530

    Leave apply changes

--- src/TestCases/TC04_ApplyLeave.java
+++ src/TestCases/TC04_ApplyLeave.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 public class TC04_ApplyLeave {
     public static void main(String[] args) throws Exception{
-        //System.setProperty("webdriver.chrome.driver", "C:\\AutomationCatalogue\\Drivers\\Chrome\\chromedriver\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver","C:\\Anitha\\AutomationCatalogue\\Drivers\\Chrome\\chromedriver_win32_1\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\AutomationCatalogue\\Drivers\\Chrome\\chromedriver\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver","C:\\Anitha\\AutomationCatalogue\\Drivers\\Chrome\\chromedriver_win32_1\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         String path = System.getProperty("user.dir");
         System.out.println("Project Path is :" + path);
@@ -38,18 +38,30 @@ public class TC04_ApplyLeave {
             throw new Exception();
         }
 
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(20));
+
         driver.findElement(By.xpath("(//span[text()='Leave'])[1]")).click();
         System.out.println("Click action is performed on Leave menu");
 
         driver.findElement(By.xpath("//a[@data-automation-id='menu_leave_applyLeave']")).click();
         System.out.println("Click action is performed on Apply under leave menu");
-        driver.findElement(By.xpath("//div[@class='select-wrapper initialized']")).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='leaveType_inputfileddiv']/div/input")));
+        driver.findElement(By.xpath("//div[@id='leaveType_inputfileddiv']/div/input")).click();
         System.out.println("Click action is performed on Leave Type");
 
-        driver.findElement(By.xpath("//div[@class='select-wrapper initialized']/ul/li[5]/span")).click();
-        System.out.println("Sick leave is selected under Leave Type");
+        String expectedLeaveType="Sick Leave - US";
+        List<WebElement> elements_leaveTypes = driver.findElements(By.xpath("//div[@id='leaveType_inputfileddiv']/div/ul/li"));
+        for(WebElement element_leave:elements_leaveTypes){
+            String leaveType=element_leave.getText();
+            if(leaveType.equalsIgnoreCase(expectedLeaveType)){
+                element_leave.click();
+                System.out.println(expectedLeaveType+" is selected as LeaveType");
+                break;
+            }
+        }
 
-        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(20));
+
 
         //leave from Date
         String from_date="17-August-2022";
@@ -101,7 +113,7 @@ public class TC04_ApplyLeave {
 
         //leave to Date
 
-        String to_dob="17-August-2022";
+        String to_dob="19-August-2022";
         String to_expectedYear=to_dob.split("-")[2];
         String to_expectedMonth=to_dob.split("-")[1];
         String to_expectedDate=to_dob.split("-")[0];
@@ -158,10 +170,4 @@ public class TC04_ApplyLeave {
 
     }
 
-    public static WebElement waitForElement(WebDriver driver, By locator, int timeout) {
-        WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(timeout));
-        //WebElement waitElement = wait1.until(ExpectedConditions.presenceOfElementLocated(locator));
-        //return waitElement;
-        return null;
-    }
 }
